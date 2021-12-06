4976cdf8e9edefc86504d54560eca70291845e00
tanabee <tanabee1986@gmail.com>
2021-12-06 21:42:44 +0900

    First commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const URL = 'https://developers.google.com/apps-script/releases'
+const CACHE_KEY = 'rss'
+
+const fetch = () => {
+  const cache = CacheService.getScriptCache()
+  const cached = cache.get(CACHE_KEY)
+  if (cached) {
+    return cached
+  }
+
+  const content = UrlFetchApp.fetch(URL).getContentText()
+  const $ = Cheerio.load(content)
+  const items = []
+  $('h3').each((i, elem) => {
+    const _date = $(elem).attr('data-text')
+    if (_date) {
+      const id = $(elem).attr('id')
+      const link = `${URL}#${id}`
+      const title = $(elem).next().text().split('\n').join()
+      const date =
+        Utilities.formatDate(new Date(_date), 'GMT', 'E, dd MMM YYYY') +
+        ' 00:00:00 GMT'
+      items.push({ link, date, title })
+    }
+  })
+  const template = HtmlService.createTemplateFromFile('rss')
+  template.items = items
+  const feed = template.evaluate().getContent()
+  cache.put(CACHE_KEY, feed, 60 * 60)
+  return feed
+}
+
+const doGet = () => {
+  return ContentService.createTextOutput(fetch()).setMimeType(
+    ContentService.MimeType.RSS
+  )
+}
